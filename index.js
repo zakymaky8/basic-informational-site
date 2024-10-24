d9286152a3ef2a4ea12e85100708c553471f6c21
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-10-24 22:27:50 +0300

    finish up the whole project

--- index.js
+++ index.js
@@ -6,12 +6,34 @@ const PORT = 8080
 
 const server = http.createServer()
 
+function getPathToFile(basename) {
+    return path.join(__dirname, "infopages", basename)
+}
+
 server.on("request", (req, res) => {
-    if (req.url === "/") {
-        fs.readFile("./infopages/index.html", (err, file) => {
-            err ? console.log(err) : res.end(file);
-        })
+    res.setHeader("content-type", "text/html");
+
+    let pathTofile = "";
+
+    switch(req.url) {
+        case "/":
+            res.statusCode = 200;
+            pathTofile = getPathToFile("index.html");
+            break
+        case "/contact-me":
+            res.statusCode =200;
+            pathTofile = getPathToFile("contact-me.html");
+            break;
+        case "/about":
+            res.statusCode = 200;
+            pathTofile = getPathToFile("about.html");
+            break;
+        default:
+            res.statusCode = 404;
+            pathTofile = getPathToFile("404.html");
     }
+
+    fs.createReadStream(pathTofile, "utf-8").pipe(res);
 })
 
 server.listen(PORT, () => {
